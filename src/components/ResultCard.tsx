1942d1d7bca82ca0d0a56711809f8c2e481e5d98
Rohan Katreddy <rohan.katreddy@gmail.com>
2024-11-24 14:51:27 -0500

    formatted Result Card

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
 
-
 export interface RequestResult {
-  prescription: string,
-  quantity: number,
-  units: string,
-  isLoading: boolean,
+  prescription: string;
+  quantity: number;
+  units: string;
+  isLoading: boolean;
   result: string;
 }
 
-function ResultCard({
-    prescription,
-    quantity,
-    units,
-    isLoading,
-    result
-}: RequestResult) {
+function ResultCard({ prescription, quantity, units }: RequestResult) {
   const [selected, setSelected] = useState(false);
-  let bgColour = selected ? " bg-success" : " bg-dark"; // change color later
+  let bgColour = selected ? " bg-info text-dark" : ""; // change color later
   return (
     <div
       className={"card p-2 m-2" + bgColour}
@@ -27,19 +20,14 @@ function ResultCard({
     >
       <h5 className="card-title text-center m-0">Pharmacy A</h5>
       <hr />
-      <div className="container row">
+      <div className="">
         <div className="container col-7">
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            {"DIN " + prescription}
+          <h6 className={" mb-2 text-body-secondary " + bgColour}>
+            {"DIN: " + prescription}
           </h6>
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            {"Units: " + units}
+          <h6 className={" mb-2 text-body-secondary " + bgColour}>
+            {"Quantity: " + quantity + " " + units}
           </h6>
-        </div>
-        <div className="container col d-flex align-items-end justify-content-end pb-2">
-          <p className="card-text text-end ">
-            {quantity}
-          </p>
         </div>
       </div>
     </div>
